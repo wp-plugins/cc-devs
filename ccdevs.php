7189434fff63b0b3499621eab266a2e226aeabc7
vegasgeek <vegasgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-16 05:13:03 +0000

    just updating the version number.
    git-svn-id: https://plugins.svn.wordpress.org/cc-devs/tags/1.0.1@1161664 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ccdevs.php
+++ ccdevs.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       CC Devs
  * Description:       Send copies of admin emails to a list of developers
- * Version:           1.0
+ * Version:           1.0.1
  * Author:            John Hawkins & Todd Huish
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
